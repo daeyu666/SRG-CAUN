ee504df6c3970cad6d7eae0c47144f03f2837f45
daeyu666 <384114681@qq.com>
2026-06-16 19:20:50 +0800

    Export hierarchical matching SRG-CAUN variant

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,12 @@
 from .srg_caun import SRGCAUN, build_srg_caun
+from .srg_caun_hier_match import SRGCAUNHierMatch, build_srg_caun_hier_match
 from .drt_baseline import DRTBaseline, build_drt_baseline
 
-__all__ = ["SRGCAUN", "build_srg_caun", "DRTBaseline", "build_drt_baseline"]
+__all__ = [
+    "SRGCAUN",
+    "build_srg_caun",
+    "SRGCAUNHierMatch",
+    "build_srg_caun_hier_match",
+    "DRTBaseline",
+    "build_drt_baseline",
+]
